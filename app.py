b5448b550fb6ef4d28af1869b317595d6f98cac8
shopycine-max <shopycine@gmail.com>
2026-09-22 14:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ def analyze_single_ticker(
           & cond_accum
           & cond_no_wick
           & cond_breakout
-
+      ) 
  else:
   cond7_252 = df['Close'] >= df['Max_252_High_1d_Ago']
   df['Signal'] = (
